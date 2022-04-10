02c7b72f24f1db0a016f07a6a6992d30f882dce4
redysp <powerc2@rpi.edu>
2022-04-10 18:23:13 -0400

    added cookies, user authentication

--- client/plantcare/src/App.js
+++ client/plantcare/src/App.js
@@ -10,33 +10,24 @@ import About from './containers/about/about'
 import Planner from './containers/planner/planner'
 import Navigation from './components/navbar/navbar'
 import useToken from './components/useToken/useToken';
+import ProtectedRoute from './components/protectedRoute/protectedRoute'
 
 
 function App() {
-  const { token, setToken } = useToken();
-
-  /* doesn't let page access until user is logged in */
-
-  // if(!token) {
-  //   return <Login setToken={setToken} />
-  // }
 
   return (
     <div className="App">
       <BrowserRouter>
       <div className="App">
         <Switch>
-            {/* <Route exact path="/">
-              <Redirect to="/Login" />
-            </Route> */}
             <Route path="/Login" exact component={Login} />
             <Route path="/Register" exact component={Register} />
             <Route path="/Forgot-Password" exact component={forgotpassword} />
           <div>
           <Navigation/>
               <Route path="/About" component={About} />
-              <Route path="/Planner" component={Planner} />
-              <Route path="/Search" component={Search} /> 
+              <ProtectedRoute path="/Planner" component={Planner} />
+              <ProtectedRoute path="/Search" component={Search} /> 
           </div> 
         </Switch>
       </div>
@@ -44,16 +35,4 @@ function App() {
     </div>
   );
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Navigation />
-//         <BrowserRouter>
-//             <Route path='/about' element={<About/>}/>
-//         </BrowserRouter>
-//     </div>
-//   );
-// }
-
 export default App;
--- client/plantcare/src/containers/planner/planner.js
+++ client/plantcare/src/containers/planner/planner.js
@@ -4,13 +4,6 @@ import useToken from '../../components/useToken/useToken';
 import Login from '../login/login';
 
 const Planner = () => { 
-    const { token, setToken } = useToken();
-
-    /* doesn't let page access until user is logged in */
-
-    if(!token) {
-        return <Login setToken={setToken} />
-    }
     return(
         <div> "Im a planner" </div>
     );
