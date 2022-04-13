bf02053ddd30f0559f2d088c589445c5f91f1775
PranjalJain-95 <pranj.jain95@gmail.com>
2022-04-12 22:00:58 -0400

    ES lint formating

--- server/db.js
+++ server/db.js
@@ -1,13 +1,13 @@
-//Author: Pranjal Jain
+// Author: Pranjal Jain
 // Code for setting up the connection to DB
-const Pool =  require('pg').Pool
+const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "plant_care",
-    password: "1234",
-    host: "localhost",
-    port: 5432,
-    database: "plant_care"
+  user: 'plant_care',
+  password: '1234',
+  host: 'localhost',
+  port: 5432,
+  database: 'plant_care',
 });
 
 module.exports = pool;
--- server/utils/jwtGenerator.js
+++ server/utils/jwtGenerator.js
@@ -1,14 +1,15 @@
-//Author: Pranjal Jain
+// Author: Pranjal Jain
 // code for generating the token using user_id
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-function jwtGenerator(user_id){
-    const payload = {
-        user: user_id
-    };
+// eslint-disable-next-line require-jsdoc
+function jwtGenerator(userId) {
+  const payload = {
+    user: userId,
+  };
 
-    return jwt.sign(payload, process.env.jwtSecret, {expiresIn: "1hr"});
+  return jwt.sign(payload, process.env.jwtSecret, {expiresIn: '1hr'});
 };
 
 module.exports = jwtGenerator;
